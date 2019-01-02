2446dcd2eed720bfdf7da1082fcf16ec6b1896fe
charles-wyman <charleswymanprogramming@gmail.com>
2019-01-02 15:32:09 -0500

    revised patterns, cleaned up html structure for more responsiveness

--- src/app/pages/contact/contact.page.ts
+++ src/app/pages/contact/contact.page.ts
@@ -1,10 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-contact',
-  templateUrl: './contact.page.html',
-  styleUrls: ['./contact.page.scss'],
+  selector: "app-contact",
+  templateUrl: "./contact.page.html",
+  styleUrls: ["./contact.page.scss"]
 })
 export class ContactPage implements OnInit {
   title: string;
@@ -14,38 +14,74 @@ export class ContactPage implements OnInit {
   subjectPattern: RegExp;
   messagePattern: RegExp;
 
-  constructor(public formBuilder: FormBuilder) { }
+  constructor(public formBuilder: FormBuilder) {}
 
   ngOnInit() {
-    this.title = 'Contact';
+    this.title = "Contact";    
     this.namePattern = /^([a-zA-Z]||'||-)+$/;
     this.emailPattern = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     this.subjectPattern = /^[aA-zZ0-9_\.\']{1,60}\@[aZ-zZ]{1,60}\.[aA-zZ]*'/;
     this.messagePattern = /^\w+$/;
 
     this.contactForm = this.formBuilder.group({
-      firstName: ['', Validators.compose([Validators.maxLength(30), Validators.pattern(this.namePattern), Validators.required])],
-      lastName: ['', Validators.compose([Validators.maxLength(30), Validators.pattern(this.namePattern), Validators.required])],
-      emailAddress: ['', Validators.compose([Validators.maxLength(30), Validators.pattern(this.emailPattern), Validators.required])],
-      subject: ['', Validators.compose([Validators.maxLength(30), Validators.pattern(this.subjectPattern), Validators.required])],
-      message: ['', Validators.compose([Validators.maxLength(30), Validators.pattern(this.emailPattern), Validators.required])]
+      firstName: [
+        "",
+        Validators.compose([
+          Validators.maxLength(30),
+          Validators.pattern("^([a-zA-Z]||'||-)+$"),
+          Validators.required
+        ])
+      ],
+      lastName: [
+        "",
+        Validators.compose([
+          Validators.maxLength(30),
+          Validators.pattern(this.namePattern),
+          Validators.required
+        ])
+      ],
+      emailAddress: [
+        "",
+        Validators.compose([
+          Validators.maxLength(30),
+          Validators.pattern(this.emailPattern),
+          Validators.required
+        ])
+      ],
+      subject: [
+        "",
+        Validators.compose([
+          Validators.maxLength(60),
+          Validators.pattern(this.messagePattern),
+          Validators.required
+        ])
+      ],
+      message: [
+        "",
+        Validators.compose([
+          Validators.maxLength(30),
+          Validators.pattern(this.messagePattern),
+          Validators.required
+        ])
+      ]
     });
   }
 
-  submit(): void {
-    const info = [{
-        'firstName' : this.contactForm.controls.firstName.value,
-        'lastName' : this.contactForm.controls.lastName.value,
-        'emailAddress ' : this.contactForm.controls.emailAddress.value,
-        'subject' : this.contactForm.controls.subject.value,
-        'message' : this.contactForm.controls.message.value
-    }];
-    console.log(info);
+  public reset(): void {
+    Object.keys(this.contactForm.controls).forEach(key => {
+      this.contactForm.get(key).setValue('');
+    });
+  }
+
+  public submit(): void {
+    const info = {};
+    Object.keys(this.contactForm.controls).forEach(key => {
+      info[key] = this.contactForm.get(key).value;
+    });
     /*
-    this.mailService.sendMail(info).then(data => {
+    this.mailService.sendMail(JSON.stringify(info)).then(data => {
         console.log('email was sent!');
     });
     */
-}
-
+  }
 }
